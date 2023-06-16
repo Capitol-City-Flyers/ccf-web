6c54c2f066566c0e6720163f05b15e7575637109
Matt Jensen <mjensen65816@gmail.com>
2023-06-15 21:33:48 -0500

    WIP: OIDC.

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,14 +1,26 @@
 import Require from "../../components/auth/Require";
 import Link from "next/link";
+import {useMemo} from "react";
+import {freeze} from "immer";
+import {OidcProvider, OidcSecure} from "@axa-fr/react-oidc";
 
 export default function() {
+    const oidcConfig = useMemo(() => ({
+        authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_Ss8A44JD9",
+        client_id: "3l9b48v7p4ef1dj33iso600mf5",
+        redirect_uri: "http://localhost:3000/members",
+        refresh_time_before_tokens_expiration_in_second: 3600,
+        scope: "email openid phone profile"
+    }), []);
     return (
-        <Require authenticate authenticated>
-            <h1 className="text-3xl font-bold underline">Profile</h1>
-            <div>
-                <h2>Links</h2>
-                <Link href="/">Home</Link>
-            </div>
-        </Require>
+        <OidcProvider configuration={oidcConfig}>
+            <OidcSecure>
+                <h1 className="text-3xl font-bold underline">Profile</h1>
+                <div>
+                    <h2>Links</h2>
+                    <Link href="/">Home</Link>
+                </div>
+            </OidcSecure>
+        </OidcProvider>
     );
 }
