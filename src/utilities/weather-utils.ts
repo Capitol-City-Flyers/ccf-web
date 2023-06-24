f172120c6af2d463af9062185bbb55fa21e848ce
Matt Jensen <matt.jensen@sansio.com>
2023-06-24 07:48:46 -0500

    WIP.

--- src/utilities/weather-utils.ts
+++ src/utilities/weather-utils.ts
@@ -1,6 +1,6 @@
+import {parseTAF} from "metar-taf-parser";
 
 export function flightCategoryIntervals(taf: string) {
-    const {parseTAF} = require("metar-taf-parser");
     const parsed = parseTAF(taf);
     parsed.trends.forEach(console.log);
 }
